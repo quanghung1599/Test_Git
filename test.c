4ca0c3e88a02c01a225015ed49f82f5f69b69e92
HungNguyen <hung.nguyen.jx@renesas.com>
2022-05-09 16:40:03 +0700

    Hung

--- test.c
+++ test.c
@@ -1,1 +1,16 @@
-sdhfpsdaiask
+#include "stdio.h"
+
+int sum(int a, int b, int c)
+{
+    return a+b+c;
+}
+
+int main()
+{
+    printf("RenesasVietNam\n");
+    int a = sum(2,3,4);
+    printf("Tong: %d\n", a);
+    
+}
+// hunghunghung
+char buff[5] = {2,3,4,5,6};
